7b4edacacb962e27f1319a1bfa3c8e45d403d23d
Gaurav Chauhan <gaurav.chauhan@kiwitech.com>
2017-09-13 17:01:11 +0530

    Setup intial project.

---
+++ app/src/main/java/com/blingby/demoselklikapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.blingby.demoselklikapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      /*  VideoListFragment videoListFragment= new VideoListFragment();
+        videoListFragment.setVideoList(MainActivity.this);
+        getFragmentManager().beginTransaction().replace(R.id.main_layout,videoListFragment).commit();*/
+    }
+}
